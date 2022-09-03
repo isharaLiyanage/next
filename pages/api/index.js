e0753ffc94168406bdcab6273e08fc0386733656
isharaLiyanage <ishandananjaya1111@gmail.com>
2022-09-03 10:31:56 +0530

    update 0.2

---
+++ pages/api/index.js
@@ -0,0 +1,10 @@
+const handler = async (req, res) => {
+  const { method } = req;
+
+  // This will allow OPTIONS request
+  if (method === "OPTIONS") {
+    return res.status(200).send("ok");
+  }
+};
+
+export default handler;

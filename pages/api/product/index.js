5ff53f3b9946c145d7f1c3eefe6290397342dcbb
isharaLiyanage <ishandananjaya1111@gmail.com>
2022-09-05 10:59:01 +0530

    dsdsds

--- pages/api/product/index.js
+++ pages/api/product/index.js
@@ -1,7 +1,14 @@
+import NextCors from "nextjs-cors";
 import dbConnect from "../../../util/mongo";
 import Product from "../../../models/Product";
 
 export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   const { method } = req;
   dbConnect();
 

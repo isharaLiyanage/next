4d2d864fdff502326f30176bff4e967b86315092
isharaLiyanage <ishandananjaya1111@gmail.com>
2022-09-05 10:28:34 +0530

    dsd

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,7 @@ export default function Home({ productList }) {
   );
 }
 export const getServerSideProps = async () => {
-  const res = await axios.get(
-    "https://incredible-biscotti-8e24c5.netlify.app/api/product"
-  );
+  const res = await axios.get("http://localhost:3000/api/product");
   return {
     props: {
       productList: res.data,
